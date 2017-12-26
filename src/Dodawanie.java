54e7a915a7331071e9c0e1d30b1037fcaba649dc
camperik94 <tomaszjach94@gmail.com>
2017-12-26 13:50:36 +0100

    Elo

--- src/Dodawanie.java
+++ src/Dodawanie.java
@@ -1,4 +1,30 @@
 
 public class Dodawanie {
+	private int liczbaA;
 
+	public int getLiczbaA() {
+		return liczbaA;
+	}
+
+	public void setLiczbaA(int liczbaA) {
+		this.liczbaA = liczbaA;
+	}
+
+	public Dodawanie(int liczbaA) {
+		this.liczbaA = liczbaA;
+	}
+
+	public Dodawanie() {
+
+	}
+
+	int suma = 0;
+
+	public void dodawanie() {
+		suma = suma + liczbaA;
+	}
+	
+	public int wypisz() {
+		return suma;
+	}
 }
--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		Dodawanie dod = new Dodawanie();
+		int liczba = 1;
 		
+		while(liczba != 0) {
+			
+			liczba = in.nextInt();
+			dod.setLiczbaA(liczba);
+			dod.dodawanie();
+		}
+		System.out.println(dod.wypisz());
+
+
 	}
 }
